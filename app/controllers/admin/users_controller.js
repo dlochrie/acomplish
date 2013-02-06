b04b0be57ecf00eb702982d43ac535544dcaaf7c
dlochrie <dclochri@dclochri-GA-MA78LMT-S2.(none)>
2013-02-05 19:20:15 -0600

    Added a sycnhronous way of getting users and their roles for the user management piece.

--- app/controllers/admin/users_controller.js
+++ app/controllers/admin/users_controller.js
@@ -6,12 +6,9 @@ before(loadAllMembers,{only: ['index']})
 
 action(function index() {
   this.title = 'Manage Users';
-  this.members.forEach(function(member) {
-   // console.log(member);
+  User.all(function (err, users) {
+    render({ registered: users });
   })
-  render({
-    usersList: this.members
-  });
 });
 
 action(function show() {
@@ -55,15 +52,33 @@ action(function destroy() {
 });
 
 function loadAllMembers() {
-  this.members = [];
-  return User.all(function(err, users) {
-    if (err || !users) {
-      next();
-    } else {
+  var usersList = [];
+  var async = require('async');
+  
+  async.waterfall([
+    function getUsers(callback) {
+      User.all(function(err, users) {
+        if (err || !users) { callback(err); }
+        return callback(null, users);
+      });
+    },
+    function getRoles(users, callback) {
       users.forEach(function(user) {
-        this.members.push(user.getRole())
-      })
-      next();
+        user.roleName = getRole(user);
+        return usersList.push(user);
+      });
+      callback(null);
     }
+  ], function(err) {
+    if (!err) {
+      render({ registered: usersList, title: "User Management" })
+    }
+  });
+}
+
+function getRole(user) {
+  Role.findOne({ where: { id: user.roleId }}, function(err, role) {
+    this.roleName = role.name;
   }.bind(this));
+  return this.roleName;
 }
