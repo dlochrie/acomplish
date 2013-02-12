054fa707ecea51fa12ae1b7c74384ddb4982ad70
dlochrie <dclochri@dclochri-GA-MA78LMT-S2.(none)>
2013-02-11 18:38:42 -0600

    Adding new files to support new Environment.

---
+++ config/database.js
@@ -0,0 +1,19 @@
+module.exports = {
+		"local": {
+        "driver": "mysql",
+        "host": "localhost",
+        "database": "acomplish",
+        "username": "dbuser",
+        "password": "dbpasswd"
+		},
+    "development": {
+        "driver": "mysql",
+        "host": "localhost",
+        "database": "acomplish",
+        "username": "dbuser",
+        "password": "dbpasswd"
+    },
+    "test": {
+        "driver": "memory"
+    }
+}
