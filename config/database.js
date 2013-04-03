3df5be5346d39a2ebba598517b32dac29975c35c
dlochrie <dclochri@dclochri-GA-MA78LMT-S2.(none)>
2013-04-03 11:26:06 -0500

    Made some formatting changes to database config.

--- config/database.js
+++ config/database.js
@@ -1,15 +1,17 @@
 module.exports = {
-		"development": {
-			"driver": "mysql",
-			"host": "localhost",
-			"database": "acomplish",
-			"username": "dbuser",
-			"password": "dbpasswd"
-		}
-  , "test": { 
-			"driver":   "memory"
-		}
-  , "production": { 
-			"driver":   "memory"
-		}
-  };
+	"development": {
+		"driver": "mysql",
+		"host": "localhost",
+		"database": "acomplish",
+		"username": "dbuser",
+		"password": "dbpasswd"
+	}
+	, "test": { 
+		"driver": "memory"
+	}
+	, "production": { 
+		"driver": "redis",
+		"host": "localhost",
+		"port": 6379
+	}
+};
