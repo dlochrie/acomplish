94326eb6717a83e900062c294f85fa19c5327c7d
dlochrie <dlochrie@somethingelse.com>
2013-06-22 21:08:32 -0500

    Updated command that runs tests (see package.json), and starting to get the tests together in working order.

--- test/controllers/users_controller_test.js
+++ test/controllers/users_controller_test.js
@@ -7,6 +7,7 @@ var app,
  * TODO: User CREATION and EDITs should be tested, with PASSPORT
  * functionality.
  */
+
 function UserStub() {
   return {
     displayName: '',
@@ -36,35 +37,6 @@ describe('UserController', function () {
         done();
       });
   });
-
-  /*
-   * GET /users/:id/edit
-   * Should access User#find and render users/edit.ejs
-   */
-
-  /*****************************************************************************
-   * This Feature is not exposed to Users yet, so the test is disabled.
-  it(
-    'should access User#find and render "edit" template on GET /users/:id/edit',
-    function (done) {
-      var User = app.models.User;
-
-      // Mock User#find
-      User.find = sinon.spy(function (id, callback) {
-        callback(null, new User);
-      });
-
-      request(app)
-        .get('/users/42/edit')
-        .end(function (err, res) {
-          res.statusCode.should.equal(200);
-          User.find.calledWith('42').should.be.true;
-          app.didRender(/users\/edit\.ejs$/i).should.be.true;
-
-          done();
-        });
-    });
-*****************************************************************************/
 
   /*
    * GET /users/:id
--- test/init.js
+++ test/init.js
@@ -11,34 +11,32 @@ global.getApp = function(done) {
   
   app.render = function (viewName, opts, fn) {
     app.renderedViews.push(viewName);
-
-      // Deep-copy flash messages
-      var flashes = opts.request.session.flash;
-      for(var type in flashes) {
-        app.flashedMessages[type] = [];
-        for(var i in flashes[type]) {
-          app.flashedMessages[type].push(flashes[type][i]);
-        }
+    // Deep-copy flash messages
+    var flashes = opts.request.session.flash;
+    for (var type in flashes) {
+      app.flashedMessages[type] = [];
+      for (var i in flashes[type]) {
+        app.flashedMessages[type].push(flashes[type][i]);
       }
-
-      return app._render.apply(this, arguments);
     }
+    return app._render.apply(this, arguments);
+  }
 
-    // Check whether a view has been rendered
-    app.didRender = function (viewRegex) {
-      var didRender = false;
-      app.renderedViews.forEach(function (renderedView) {
-        if(renderedView.match(viewRegex)) {
-          didRender = true;
-        }
-      });
-      return didRender;
-    }
+  // Check whether a view has been rendered
+  app.didRender = function (viewRegex) {
+    var didRender = false;
+    app.renderedViews.forEach(function (renderedView) {
+      if(renderedView.match(viewRegex)) {
+        didRender = true;
+      }
+    });
+    return didRender;
+  }
 
-    // Check whether a flash has been called
-    app.didFlash = function (type) {
-      return !!(app.flashedMessages[type]);
-    }
+  // Check whether a flash has been called
+  app.didFlash = function (type) {
+    return !!(app.flashedMessages[type]);
+  }
 
-    return app;
-  };
+  return app;
+};
